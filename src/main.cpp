b09f8df264577a27f84024931283c3f9f73cc00a
krish choudhary <krish.choudhary_cs.aiml24@gla.ac.in>
2026-02-20 16:00:08 +0530

    Define RGB LED pin constants

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,14 @@
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
  */
+// TODO 1:
+// Define RED pin (Use 9)
+#define RED_PIN 9
+
+// TODO 2:
+// Define GREEN pin (Use 10)
+#define GREEN_PIN 10
+
+// TODO 3:
+// Define BLUE pin (Use 11)
+#define BLUE_PIN 11
